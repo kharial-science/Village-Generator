96f395dc58d77ac95af1282cddfaf1b8252a40c0
Kharoh <Gaulu03@gmail.com>
2020-04-16 00:40:08 +0200

    Updates

---
+++ precache-manifest.b3db2347b7dc062f89f380e210b5852f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d625fcc5fa2be44bf9b80de651fef3",
+    "url": "/Village-Generator/index.html"
+  },
+  {
+    "revision": "c4fc4c81913c993f0dea",
+    "url": "/Village-Generator/static/css/main.0e184008.chunk.css"
+  },
+  {
+    "revision": "d6868a037c4d9ce0417b",
+    "url": "/Village-Generator/static/js/2.5faa804b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Village-Generator/static/js/2.5faa804b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4fc4c81913c993f0dea",
+    "url": "/Village-Generator/static/js/main.6c73ee53.chunk.js"
+  },
+  {
+    "revision": "cedd4ab90950773a55ab",
+    "url": "/Village-Generator/static/js/runtime-main.2ae2ba5e.js"
+  },
+  {
+    "revision": "0e087ae0f0db428579b207824ae5da93",
+    "url": "/Village-Generator/static/media/building_houseBlue.0e087ae0.svg"
+  },
+  {
+    "revision": "b5adbf1e431ee89f631315873585af9f",
+    "url": "/Village-Generator/static/media/building_houseGreen.b5adbf1e.svg"
+  },
+  {
+    "revision": "e7a95da15f1deb7b42575489b642dd4b",
+    "url": "/Village-Generator/static/media/building_housePink.e7a95da1.svg"
+  },
+  {
+    "revision": "8639dadd93234d6eb4160cbc097cf7dd",
+    "url": "/Village-Generator/static/media/building_houseYellow.8639dadd.svg"
+  },
+  {
+    "revision": "ec27b5979a37d3ef320b10dc68979fe4",
+    "url": "/Village-Generator/static/media/familybuilding_townhall.ec27b597.svg"
+  },
+  {
+    "revision": "cca8f218e30118897041aaf963b649ec",
+    "url": "/Village-Generator/static/media/global_background.cca8f218.svg"
+  },
+  {
+    "revision": "863d513a2f9c5486ced1c7a0053f2fca",
+    "url": "/Village-Generator/static/media/global_chooseFamilyLogo.863d513a.svg"
+  },
+  {
+    "revision": "a15a5b3a6475548ca7a36e207276c15d",
+    "url": "/Village-Generator/static/media/global_way.a15a5b3a.svg"
+  }
+]);
