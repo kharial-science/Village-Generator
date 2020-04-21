21b7dae16e2ee4a6b6c6912186a4e1b2b7bd23e9
Jerryh2-0 <supermighty0.cubeway2.mcpe@gmail.com>
2020-04-21 11:25:01 +0200

    added personalized townhall

--- src/components/Map/Chunk.js
+++ src/components/Map/Chunk.js
@@ -9,7 +9,7 @@ class Chunk extends Component {
     console.log(this.props.building)
     return (
       <div className={`Chunk ${this.props.isSelected ? 'selected' : ''}`} onClick={() => this.props.handleChunkSelect(this.props.index)}>
-        {this.props.building ? <img src={this.props.building === 'house' ? require(`../../assets/images/building_house${this.props.family.charAt(0).toUpperCase() + this.props.family.slice(1)}.svg`) : this.props.building === 'townhall' ? require('../../assets/images/familybuilding_townhall.svg') : require('../../assets/images/global_way.svg')} /> : ''}
+        {this.props.building ? <img src={this.props.building === 'house' ? require(`../../assets/images/building_house${this.props.family.charAt(0).toUpperCase() + this.props.family.slice(1)}.svg`) : this.props.building === 'townhall' ? require(`../../assets/images/familybuilding_townhall${this.props.family.charAt(0).toUpperCase() + this.props.family.slice(1)}.svg`) : require('../../assets/images/global_way.svg')} /> : ''}
       </div>
     )
   }
